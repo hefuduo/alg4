6147d6297857a55811b4b96c97fd1c21ddcd09b4
hefuduo <hefuduo@foxmail.com>
2021-05-20 20:19:13 +0800

    id:ONES-111111 cause:add abstract class ones_ref: solution:done
    Signed-off-by: hefuduo <hefuduo@foxmail.com>

---
+++ src/main/java/j/edu/princeton/cs/cp2/AbstractSort.java
@@ -0,0 +1,32 @@
+package j.edu.princeton.cs.cp2;
+
+import java.util.Arrays;
+
+/**
+ * j.edu.princeton.cs.cp2
+ * Created by hefuduo on 5/20/21.
+ */
+public abstract class AbstractSort {
+
+    protected Comparable[] array;
+
+    public abstract void sort();
+
+    public static boolean less(Comparable a, Comparable b) {
+        return a.compareTo(b) < 0;
+    }
+
+    public boolean isSorted() {
+        for (int i = 1; i < array.length; i++) {
+            if (less(array[i], array[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void show() {
+        String s = Arrays.toString(array);
+        System.out.println(s);
+    }
+}
